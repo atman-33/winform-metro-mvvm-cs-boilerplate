54081dc2bf9ecbb66ae97738bbf872a67fd17a14
atman <gpbjk0304@gmail.com>
2024-04-27 15:04:33 +0900

    feat: log4netを追加

--- apps/WinFormBoilerplate/WinFormBoilerplate.WinForm/Program.cs
+++ apps/WinFormBoilerplate/WinFormBoilerplate.WinForm/Program.cs
@@ -5,6 +5,11 @@ namespace WinFormBoilerplate.WinForm
     internal static class Program
     {
         /// <summary>
+        /// ログ
+        /// </summary>
+        private static log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType);
+
+        /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
@@ -13,6 +18,13 @@ namespace WinFormBoilerplate.WinForm
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            _logger.Debug("デバッグのログ");
+            _logger.Info("インフォのログ");
+            _logger.Warn("警告のログ");
+            _logger.Error("エラーのログ");
+            _logger.Fatal("致命的なログ");
+
             Application.Run(new HomeView());
         }
     }
