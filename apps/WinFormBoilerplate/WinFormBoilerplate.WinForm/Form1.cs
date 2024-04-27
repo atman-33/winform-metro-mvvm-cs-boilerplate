ee5c5f4dee67c7cae5c3f7e4101e89ae8f355c59
atman <gpbjk0304@gmail.com>
2024-04-27 12:48:36 +0900

    feat: MetroFormを適用

--- apps/WinFormBoilerplate/WinFormBoilerplate.WinForm/Form1.cs
+++ apps/WinFormBoilerplate/WinFormBoilerplate.WinForm/Form1.cs
@@ -1,6 +1,8 @@
+using MetroFramework.Forms;
+
 namespace WinFormBoilerplate.WinForm
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
